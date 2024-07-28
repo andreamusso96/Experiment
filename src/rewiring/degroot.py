ede426f8e4ac1a185e83e7da98f42f0159d8598e
andreamusso96 <andreamusso96@gmail.com>
2024-07-28 22:15:07 +0200

    De groot version2: sweep through possible rewiring and select the best option

--- src/rewiring/degroot.py
+++ src/rewiring/degroot.py
@@ -1,74 +1,118 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, List, Dict
 import numpy as np
 import copy
+import networkx as nx
+from tqdm import tqdm
 
 
-def degroot_err(m: np.ndarray, initial_belief: np.ndarray, correct_belief: float, niter: int) -> float:
-    m = m / np.sum(m, axis=1)
-    mt = np.linalg.matrix_power(m, niter)
+def degroot_err(m: np.ndarray, initial_belief: np.ndarray, correct_belief: float, niter: int) -> Tuple[float, Union[None, np.ndarray]]:
+    m_ = m.copy()
+    div = m_.sum(axis=1)
+    m_ = m_ * (1 / div).reshape(-1, 1)
+    mt = np.linalg.matrix_power(m_, niter)
+    if np.sum(np.abs(mt - np.linalg.matrix_power(mt, 10))) > 1e-6:
+        return np.inf, None
     pt = mt @ initial_belief
-    error = np.sum(np.square(pt - correct_belief))
-    return error
+    error = 1/m.shape[0] * np.sum(np.abs(pt - correct_belief))
+    return error, pt
 
 
 class Result:
-    def __init__(self, m: np.ndarray, m0: np.ndarray, error: float, initial_error: float, converged: bool, iterations: int):
+    def __init__(self, m: np.ndarray, m0: np.ndarray, error: float, initial_error: float, converged: bool, iterations: int, trajectory: Dict):
         self.m = m
         self.m0 = m0
         self.error = error
         self.initial_error = initial_error
         self.converged = converged
         self.iterations = iterations
+        self.trajectory = trajectory
 
 
-def myopic_search(m0: np.ndarray, niter_search: int, convergence_bar: int, max_edge_value: int, initial_belief: np.ndarray, correct_belief: float, niter_degroot: int):
+def myopic_search(m0: np.ndarray, niter_search: int, max_edge_value: int, initial_belief: np.ndarray, correct_belief: float, niter_degroot: int):
     m = m0.copy()
-    initial_error = degroot_err(m=m, initial_belief=initial_belief, correct_belief=correct_belief, niter=niter_degroot)
+    initial_error, final_belief = degroot_err(m=m, initial_belief=initial_belief, correct_belief=correct_belief, niter=niter_degroot)
 
-    n_rounds_no_improvement = 0
-    iteration = 0
+    iteration = 1
     current_error = copy.deepcopy(initial_error)
     converged = False
+    trajectory = {0: {'m': m0, 'final_belief': final_belief}}
 
     for i in range(niter_search):
+        nodes_random_order = np.random.permutation(m0.shape[0])
+        improvement_made = False
 
-        i, j = get_random_edge(m)
-        k = get_target_of_edge_rewire(i, m)
-        if k is None:
-            continue
+        for v in nodes_random_order:
+            sources = np.argwhere(m[v] > 0).flatten()
+            targets = np.argwhere(m[v] == 0).flatten()
+            targets = targets[targets != v]
 
-        m_rewire = m.copy()
-        m_rewire[i, j] = 0
-        m_rewire[i, k] = np.random.randint(1, max_edge_value + 1)
-        error_rewire = degroot_err(m=m_rewire, initial_belief=initial_belief, correct_belief=correct_belief, niter=niter_degroot)
+            best_m, best_error, best_belief = get_optimal_rewire(m=m, v=v, sources=sources, targets=targets, max_edge_value=max_edge_value, initial_belief=initial_belief, correct_belief=correct_belief, niter_degroot=niter_degroot)
 
-        if error_rewire < current_error:
-            m = m_rewire
-            current_error = error_rewire
-            n_rounds_no_improvement = 0
-        else:
-            n_rounds_no_improvement += 1
+            if best_error < current_error:
+                m = best_m.copy()
+                current_error = best_error
+                improvement_made = True
+                trajectory[iteration] = {'m': m.copy(), 'final_belief': best_belief}
 
-        iteration += 1
-        if n_rounds_no_improvement > convergence_bar:
+            iteration += 1
+
+        if not improvement_made or current_error < 1e-3:
             converged = True
-            break
 
-    result = Result(m=m, m0=m0, error=current_error, initial_error=initial_error, converged=converged, iterations=iteration - n_rounds_no_improvement)
+    result = Result(m=m, m0=m0, error=current_error, initial_error=initial_error, converged=converged, iterations=iteration, trajectory=trajectory)
     return result
 
 
-def get_random_edge(m: np.ndarray) -> Union[Tuple[int, int], Tuple[None, None]]:
-    indices_non_zero = np.argwhere(m > 0)
-    index_non_zero = np.random.choice(np.arange(len(indices_non_zero)))
-    i, j = indices_non_zero[index_non_zero]
-    return i, j
+def get_optimal_rewire(m: np.ndarray, v: int, sources: np.ndarray, targets: np.ndarray, max_edge_value: int, initial_belief: np.ndarray, correct_belief: float, niter_degroot: int) -> Tuple[np.ndarray, float, np.ndarray]:
+    best_m = None
+    best_error = np.inf
+    best_belief = None
+    for s in sources:
+        for t in targets:
+            for ev in range(1, max_edge_value + 1):
+                m_rewire = m.copy()
+                m_rewire[v, s] = 0
+                m_rewire[v, t] = ev
+                error_rewire, final_belief = degroot_err(m=m_rewire, initial_belief=initial_belief, correct_belief=correct_belief, niter=niter_degroot)
+
+                if error_rewire < best_error:
+                    best_m = m_rewire
+                    best_error = error_rewire
+                    best_belief = final_belief
+
+    return best_m, best_error, best_belief
 
 
-def get_target_of_edge_rewire(i: int, m: np.ndarray) -> Union[int, None]:
-    indices_zero = np.argwhere(m[i] == 0).flatten()
-    if len(indices_zero) == 1:  # Just the self-loop
-        return None
-    index_zero = np.random.choice(np.arange(len(indices_zero)))
-    k = int(indices_zero[index_zero])
-    return k
+if __name__ == '__main__':
+    dims = 10
+    erdos_reni_p = 0.5
+    n_initial_conditions_attempts = 100
+    max_edge_value = 4
+    init_belief = np.random.rand(dims)
+    correct_belief = 0.5
+    niter_degroot = 1000
+    niter_search = 10_000
+    convergence_bar = 100
+
+
+    def generate_initial_conditions(n: int, p: float, n_attempts: int):
+        initial_conditions = []
+        for i in range(n_attempts):
+            m = nx.adjacency_matrix(nx.erdos_renyi_graph(n=n, p=p)).todense()
+            if np.min(np.sum(m, axis=1)) == 0 or np.max(np.sum(m, axis=1)) == n - 1:
+                continue
+
+            m = np.random.randint(1, max_edge_value + 1, size=(n, n)) * m
+            initial_conditions.append(m)
+
+        unique_initial_conditions = np.unique([s.flatten() for s in initial_conditions], axis=0)
+        unique_initial_conditions = [s.reshape(dims, dims) for s in unique_initial_conditions]
+        print('Number of unique initial conditions:', len(unique_initial_conditions))
+        return unique_initial_conditions
+
+    init_cond = generate_initial_conditions(n=dims, p=erdos_reni_p, n_attempts=n_initial_conditions_attempts)
+
+    results = []
+    for x in tqdm(init_cond):
+        res = myopic_search(m0=x, niter_search=niter_search, convergence_bar=convergence_bar, max_edge_value=max_edge_value, initial_belief=init_belief, correct_belief=correct_belief, niter_degroot=niter_degroot)
+        results.append(res)
